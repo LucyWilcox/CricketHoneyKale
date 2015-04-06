bcd25cc79365c4957aa2b3463e188d8a694b95c4
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-04-06 09:26:51 -0400

    adding saladtoppings

---
+++ saladtoppings.py
@@ -0,0 +1,8 @@
+beans = ['garbanzo beans', 'kidney beans', 'lima beans', 'black eye peas', 'soy beans']
+fruit = ['apple', 'pinapple', 'strawberries', 'pomegranate seeds', 'pear', 'cranberries', 'coconut', 'dates', 'orange', 'rasins', 'watermelon balls', 'cantaloupe balls', 'honeydew balls', 'grapes', 'kiwi', 'lemon wedges']
+nuts = ['almonds', 'sunflower seeds', 'walnuts', 'sesame seeds', 'cashews', 'pine nuts', 'peanuts', 'pecans']
+vegtables = ['cabbage', 'tomatoes', 'water cress', 'beets', 'pickled beets', 'eggplant', 'yam', 'zucchini', 'cherry tomatoes', 'spaghetti squash', 'sweet peas', 'sweet potato', 'radishes', 'snow peas', 'peppers', 'pickled onions', 'jimica', 'onions', 'olives', 'jalapenos', 'mushrooms', 'cauliflower', 'celery', 'cucumber', 'alfalfa sprouts', 'asparagus', 'avocado', 'beets', 'broccoli', 'bean sprouts', 'brussel sprouts', 'corn', 'baby corn', 'carrots']
+meat = ['bacon bits', 'tofu', 'beef', 'tuna', 'fish', 'steak', 'ham', 'chicken', 'turkey', 'shrimp', 'crab meat', 'salami', 'ham']
+dariy = ['jack', 'goat cheese', 'gorgonzola', 'parmesan', 'mozzarella', 'cheddar', 'feta cheese', 'blue cheese', 'provolone', 'cottage cheese', 'hard boiled egg']
+herbs = ['cilantro', 'flowers', 'garlic', 'basil', 'mint', 'ginger', 'pepper', 'salt']
+carbs = ['fritos', 'crispy noodles', 'tortilla strips', 'croutons', ]
