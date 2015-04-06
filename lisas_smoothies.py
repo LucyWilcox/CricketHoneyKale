bd3e9f4ef2a4ad366c3860d1e32c1fbaeae77cf5
li <lisa.hachmann@gmail.com>
2015-04-06 16:41:29 -0400

    Creates smoothies using classes

---
+++ lisas_smoothies.py
@@ -0,0 +1,18 @@
+from smoothies import smoothie_ingredients
+from random import choice
+
+class Smoothie(object):
+	def __init__(self):
+		self.create_random_smoothie(4)
+#		self.ingredients = ingredients
+
+	def __str__(self):
+		return "This smoothie includes %s" %(self.ingredients)
+
+	def create_random_smoothie(self,num_ingredients):
+		self.ingredients = []
+		for i in range(num_ingredients):
+			self.ingredients.append(choice(smoothie_ingredients))
+
+peaches = Smoothie()
+print peaches
