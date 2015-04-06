27080f68450275559dc2fdbccc1baa6222dc430c
LoganSweet <logan.sweet@students.olin.edu>
2015-04-06 16:16:40 -0400

    soup ingredients

---
+++ souplist.py
@@ -0,0 +1,16 @@
+base = [['chicken broth','chicken stock'],['beef broth','beef stock'],['vegetable broth','vegetable stock]',['cream','milk'],'water','coconut milk','egg noodles']
+fat = ['butter','vegetable oil','canola oil','olive oil','']
+starch = ['toast','potato','sweet potato','butternut squash','couscous',['pasta','noodles','elbows','penne','bow ties',''],'tortilla strips','rice','glass noodles','rice noodles','','','']
+meat = ['mussels',['beef','sirloin','roast'],'pork','sausage','chorizo','chicken','turkey','clams','bacon','','','','','','']
+spices = ['chili powder','white wine','red wine','brown sugar','curry','basil','rosemary','cumin','garlic','cinnamon','ginger','cloves','fennel','thyme','saffrom','','','','']
+vegeatbles = ['tomato','zucchini','kale','cabbage','spinach','corn','carrots','onion','red onion','white onion','yellow onion','tomatillos','celery','bell pepper','broccoli','','','','','','']
+legumes = ['kidney beans','black beans','peas','lima beans','lentils','jalapeno','seaweed']
+toppings = ['cheese','sour cream','cilantro','lime','green onions','avocado']
+soupingredients = base + fat + starch + meat + spices + vegetables + legumes + toppings
+
+
+#Sample instructions: 
+#In a heavy bottomed pot, cook "vegatables" and "meat" in the "fat". 
+#Add "base" and "spices" and simmer for "time". 
+#Stir in "starch" and "legumes" and simmer for "time". 
+#Serve in bowls with "topping". 
