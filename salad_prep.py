3dc725a3cd59b67cfe0b003b2d5d20b816e3fecf
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-04-12 17:18:24 -0400

    working of making prep methods into a class

--- salad_prep.py
+++ salad_prep.py
@@ -1,68 +1,130 @@
 from saladtoppings import *
-from database_of_recipies import *
+from database_of_recipies import recipes
 import nltk
 import re
 from collections import Counter
 import pickle
 
 
+class PrepDict(object):
 
-def raw_list():
-	each_ingredient = []
-	for recipe in recipes:
-		seperated_ingredients = recipe.ingredients.replace(',', '').replace(']','').split("*")
-		each_ingredient.extend(seperated_ingredients)
-	each_ingredient = filter(lambda a: a != '[', each_ingredient)
-	return each_ingredient
+	def __init__(self, recipes, ingredients_used, file_name):
+		self.recipes = recipes
+		self.ingredients_used = ingredients_used
+		self.file_name = file_name
 
+	def get_raw_list(self):
+		each_ingredient = []
+		for recipe in self.recipes:
+			seperated_ingredients = recipe.ingredients.replace(',', '').replace(']','').split("*")
+			each_ingredient.extend(seperated_ingredients)
+		each_ingredient = filter(lambda a: a != '[', each_ingredient)
+		self.raw_list = each_ingredient
 
-def each_prep_methods(raw_list, salad_ingredients):
-	good_pos = ['VBN']
-	text = nltk.word_tokenize(raw_list)
-	tags = nltk.pos_tag(text)
-	good_types = []
-	for tag in tags:
-		if tag[1] in good_pos and tag[0] not in salad_ingredients:
-			good_types.append(tag)
-	return good_types
+	def full_method_dict(self):
+		def each_prep_methods(self):
+			good_pos = ['VBN']
+			text = nltk.word_tokenize(self.each_ingredient)
+			tags = nltk.pos_tag(text)
+			good_types = []
+			for tag in tags:
+				if tag[1] in good_pos and tag[0] not in self.ingredients_used:
+					good_types.append(tag)
+			return good_types
+		method_dict = dict.fromkeys(self.ingredients_used)
+		for method in self.raw_list:
+			for key in method_dict:
+				if key in method:
+					tags = each_prep_methods(self)
+					if tags != []:
+						if key == tags[0]:
+							pass
+						elif method_dict[key] == None:
+							method_dict[key] = tags
+						else:
+							method_dict[key].extend(tags)
+		self.long_dict = method_dict
 
+	def get_top_methods(self):
+		for key in self.long_dict:
+			methods = long_dict[key]
+			VBN = []
+			if methods != None:
+				for method in methods:
 
-def full_method_dict(raw_list, salad_ingredients):
-	salad_dict = dict.fromkeys(salad_ingredients)
-	for method in raw_list:
-		for key in salad_dict:
-			if key in method:
-				tags = each_prep_methods(method, salad_ingredients)
-				if tags != []:
-					if key == tags[0]:
-						pass
-					elif salad_dict[key] == None:
-						salad_dict[key] = tags
-					else:
-						salad_dict[key].extend(tags)
-	return salad_dict
-
-
-def get_top_methods(long_dict):
-	for key in long_dict:
-		methods = long_dict[key]
-		VBN = []
-		if methods != None:
-			for method in methods:
-
-				VBN.append(method[0])
-			cVBN = Counter(VBN)
-			topVBN = [ite for ite, it in cVBN.most_common(1)]
-			long_dict[key] = topVBN[0]
-	return long_dict
-
-
-def pickle_it(short_dict):
-	with open('saladmethoddict.pickle', 'wb') as handle:
-		pickle.dump(short_dict, handle)
+					VBN.append(method[0])
+				cVBN = Counter(VBN)
+				topVBN = [ite for ite, it in cVBN.most_common(1)]
+				long_dict[key] = topVBN[0]
+		self.short_dict = long_dict
 
 
-raw_list = raw_list()
-long_dict = full_method_dict(raw_list, salad_ingredients)
-short_dict = get_top_methods(long_dict)
-pickle_it(short_dict)
+	def pickle_it(self):
+		with open(self.file_name, 'wb') as handle:
+			pickle.dump(self.short_dict, handle)
+
+
+salad = PrepDict(recipes, salad_ingredients, 'saladmethoddict.pickle')
+salad.get_raw_list()
+salad.full_method_dict()
+salad.get_top_methods()
+print salad.short_dict
+# def raw_list():
+# 	each_ingredient = []
+# 	for recipe in recipes:
+# 		seperated_ingredients = recipe.ingredients.replace(',', '').replace(']','').split("*")
+# 		each_ingredient.extend(seperated_ingredients)
+# 	each_ingredient = filter(lambda a: a != '[', each_ingredient)
+# 	return each_ingredient
+
+
+# def each_prep_methods(raw_list, salad_ingredients):
+# 	good_pos = ['VBN']
+# 	text = nltk.word_tokenize(raw_list)
+# 	tags = nltk.pos_tag(text)
+# 	good_types = []
+# 	for tag in tags:
+# 		if tag[1] in good_pos and tag[0] not in salad_ingredients:
+# 			good_types.append(tag)
+# 	return good_types
+
+
+# def full_method_dict(raw_list, salad_ingredients):
+# 	salad_dict = dict.fromkeys(salad_ingredients)
+# 	for method in raw_list:
+# 		for key in salad_dict:
+# 			if key in method:
+# 				tags = each_prep_methods(method, salad_ingredients)
+# 				if tags != []:
+# 					if key == tags[0]:
+# 						pass
+# 					elif salad_dict[key] == None:
+# 						salad_dict[key] = tags
+# 					else:
+# 						salad_dict[key].extend(tags)
+# 	return salad_dict
+
+
+# def get_top_methods(long_dict):
+# 	for key in long_dict:
+# 		methods = long_dict[key]
+# 		VBN = []
+# 		if methods != None:
+# 			for method in methods:
+
+# 				VBN.append(method[0])
+# 			cVBN = Counter(VBN)
+# 			topVBN = [ite for ite, it in cVBN.most_common(1)]
+# 			long_dict[key] = topVBN[0]
+# 	return long_dict
+
+
+# def pickle_it(short_dict):
+# 	with open('saladmethoddict.pickle', 'wb') as handle:
+# 		pickle.dump(short_dict, handle)
+
+
+# raw_list = raw_list()
+# long_dict = full_method_dict(raw_list, salad_ingredients)
+# short_dict = get_top_methods(long_dict)
+# pickle_it(short_dict)
