426b7dec14794f049f9cc0e983238e5ddd51086e
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-04-12 12:40:04 -0400

    done at lunch meeting

--- saladtoppings.py
+++ saladtoppings.py
@@ -5,8 +5,8 @@ vegtables = ['cabbage', 'tomatoes', 'water cress', 'beets', 'eggplant', 'yam', '
 meat = ['bacon', 'tofu', 'beef', 'tuna', 'fish', 'salmon', 'steak', 'ham', 'chicken', 'sausage', 'turkey', 'shrimp', 'crab meat', 'salami', 'ham', 'anchovy']
 dariy = ['jack', 'goat cheese', 'gorgonzola', 'parmesan', 'mozzarella', 'cheddar', 'feta cheese', 'blue cheese', 'provolone', 'cottage cheese', 'hard boiled egg', 'egg']
 herbs = ['cilantro', 'flowers', 'garlic', 'basil', 'mint', 'ginger', 'pepper', 'parsley', 'scallion', 'cayenne pepper']
-carbs = ['fritos', 'crispy noodles', 'tortilla strips', 'croutons', 'bread','couscous']
-weird_dressings = ['soy sauce', 'sesame oil', 'chili sauce', 'salsa']
+carbs = ['crispy noodles', 'tortilla strips', 'croutons', 'bread','couscous', 'quinoa']
+weird_dressings = ['soy sauce', 'sesame oil', 'chili sauce']
 dressing = ['ranch', 'honey mustard', 'balsamic vinegar', 'italian dressing', 'caesar dressing', 'thousand island dressing', 'blue cheese dressing']
 salad_ingredients = beans + fruit + nuts + weird_dressings + vegtables + meat + dariy + herbs + carbs
 
