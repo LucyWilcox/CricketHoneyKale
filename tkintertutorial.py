f7713521d8627149fff412c7446817c933817804
li <lisa.hachmann@gmail.com>
2015-04-12 23:11:17 -0400

    More Buttons

--- tkintertutorial.py
+++ tkintertutorial.py
@@ -4,6 +4,7 @@ class Application(Frame):
 	def __init__(self, master):
 		self.master = master
 		self.page_frame = Frame(self.master, width = 1000, height = 1000, colormap = 'new')
+	#	self.next_frame = Frame(self.master, width = 500, height = 300, colormap = 'new')
 		self.Opening_Page()
 		self.page_frame.pack(fill = X)
 
@@ -12,15 +13,17 @@ class Application(Frame):
 	# """Commented code from making buttons"""
 
 	def Opening_Page(self):
-	 	Button(self.page_frame, text='Salad').pack(fill = X)#,command=self.Next_Page()) 
-	 	Button(self.page_frame, text='Quit',command=quit).pack(fill = X) 
-
+	 	Button(self.page_frame, text='Salad').pack(fill = BOTH, expand = 1)#,command=self.Next_Page()).pack(fill = X)
+	 	Button(self.page_frame, text= 'Soup').pack(fill = X, expand = 1)
+	 	Button(self.page_frame, text= "Smoothie").pack(fill = X, expand = 1)
+	 	Button(self.page_frame, text = "Info").pack(fill = X, expand = 1)
+	 	Button(self.page_frame, text='Quit',command=quit).pack(fill = BOTH, expand = 1) 
 	"""Commented code to have an error button show up with a certain else-
 	do not delete"""
 	# def Next_Page(self):
-	# #	self.nextbutton = tk.Button(self, text = 'Next')
-	# 	self.page_frame = Frame(self.master)
-	# def Error_popup_message(self):
+	# 	Button(self.next_frame, text = 'Try Again', command = quit).pack(fill = X)
+
+	#def Error_popup_message(self):
  #   		tkMessageBox.showinfo("Recipe Generator", "No recipes found, try again")
 
 if __name__ == '__main__':
