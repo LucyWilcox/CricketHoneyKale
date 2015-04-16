8e561a8b3a00c04e91e745f1ece90de0c4069bb3
li <lisa.hachmann@gmail.com>
2015-04-16 15:55:27 -0400

    level two

--- dangerfactor.py
+++ dangerfactor.py
@@ -14,7 +14,7 @@ from smoothies import smoothie_ingredients
 from soupingredientsstandard import soup_ingredients
 from salad import salad_ingredients
 from random import choice, randint
-
+food_type_databases = [smoothie_ingredients, soup_ingredients, salad_ingredients]
 
 class DangerFactor(object):
     def __init__(self, current_recipe, danger_level):
@@ -24,32 +24,42 @@ class DangerFactor(object):
             self.level_zero()
         elif self.danger_level == 1:
             self.level_one()
+        elif self.danger_level == 2:
+            self.level_two()
     def level_zero(self):
         self.new_ingredients = self.current_recipe.toppings
 
     def level_one(self):
-        """Picks a random url and gets the ingredients and instructions. No 
-        extra code needed"""
+        """Swaps one ingredient with a random choice within food_type"""
         swap_spot = randint(0,len(self.current_recipe.toppings)-1)
         if self.current_recipe.recipe_type == 'soup':
             new_ingredient = choice(soup_ingredients)
             self.current_recipe.toppings[swap_spot] = new_ingredient
+        if self.current_recipe.recipe_type == 'smoothie':
+            new_ingredient = choice(smoothie_ingredients)
+            self.current_recipe.toppings[swap_spot] = new_ingredient
+        if self.current_recipe.recipe_type == 'salad':
+            new_ingredient = choice(salad_ingredients)
+            self.current_recipe.toppings[swap_spot] = new_ingredient
         self.new_ingredients = self.current_recipe.toppings
 
     def level_two(self):
-        """Calls the makerecipe() function of salad.py twice to make two recipes. 
-        If after comparing the two, it finds any similarities, it replaces the 
-        similarities with random parts of the other recipe. 
-        """
-        pass
+        """Replace an ingredient with a random ingredient without
+        regarding food_type"""
+        swap_spot = randint(0,len(self.current_recipe.toppings)-1)
+        extra_ingredient_type = choice(food_type_databases)
+        new_ingredient = choice(extra_ingredient_type)
+        self.current_recipe.toppings[swap_spot] = new_ingredient
+        self.new_ingredients = self.current_recipe.toppings
 
     def level_three(self):
-        """Replaces any similar ingredients of 2 recipes of the same food type with
-        random choices between the three. If there are no similarities, it 
-        replaces random ones with random NEW types of ingredients"""
+        """Replaces 2 ingredients in the recipe: one being in the food type 
+        and one danger ingredient"""
         pass
+        
     def level_four(self):
-        """Introduces another random ingredient without regarding food_type"""
+        """Replaces 2 ingredients in the recipe: one not specifically in the 
+        food type, and one danger ingredient"""
         pass
 
     def level_five(self):
