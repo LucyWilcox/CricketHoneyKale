7c28ce2b28eb1f32897f49bb1a5786cc3a3c74e9
LoganSweet <logan.sweet@students.olin.edu>
2015-04-21 15:44:26 -0400

    gui today

--- GUIlogan2.py
+++ GUIlogan2.py
@@ -28,20 +28,25 @@ class Application(tk.Frame):
         #danger = tk.Scale(self.master, from_=0, to=5)
         danger = tk.Spinbox(self.master, from_=0, to=5)
         #danger = tk.Scrollbar(self.master)
+        instructions = tk.Label(self.master, text = 'INSTRUCTIONS GO HERE OR SOMETHING')
+        bottom = tk.Label(self.master, text = ' ')
 
         
 
         def placeWidgets():
-            feedback.grid(row=5,column=0,columnspan=4, pady=20)
+            feedback.grid(row=6,column=0,columnspan=4, pady=20)
             title.grid(row=0, column=1, padx=20, columnspan=2)
             quit.grid(row=0, column=3, padx=20, pady=30)
-            disclaimer.grid(row=4,column=0,columnspan=4)
+            disclaimer.grid(row=5,column=0,columnspan=4)
             danger.grid(row=3,column=1,columnspan=2, pady=30)
 
             soup.grid(row=1,column=0, padx=20)
             salad.grid(row=1,column=1, padx=20)
             smoothie.grid(row=1,column=2, padx=20)
             sandwich.grid(row=1,column=3, padx=20)
+
+            instructions.grid(row=4, column=0, columnspan=4)
+            #bottom.grid(row = 7, pady = 15)
 
         placeWidgets()
 
