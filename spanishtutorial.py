9afa917529b2a2104228dab7aeb6af014f793781
LoganSweet <logan.sweet@students.olin.edu>
2015-04-28 22:32:40 -0400

    gui example for spinbox

--- spanishtutorial.py
+++ spanishtutorial.py
@@ -6,32 +6,37 @@ class Application(tk.Frame):
         tk.Frame.__init__(self, master)
         self.grid()  
         self.master = master
-        self.run()
-        #self.placeWidgets()
+        
+        self.w()
+    	self.box()
 
 
-    def danger(self):
+    def w(self):
     	disclaimer = tk.Label(self.master, text = "\n\nPlease be safe. \n We are not responsible for food poisioning, allergic reactions, broken blenders, gross recipes or anything else.")
         disclaimer.grid(row=5,column=0,columnspan=4, pady=10)
 
+    def box(self):     
 
-    def box(self):
-    	quit = tk.Button(self.master, text='get dangerous',command=self.obtener)
-        quit.grid(row=0, column=3, padx=20, pady=30)
+        #self.value = " abc"
+        value = tk.StringVar()
 
         dangerbox = tk.Spinbox(self.master,textvariable=value,from_=0, to=5)
         dangerbox.grid(row=0, column=2)
 
+        but = tk.Button(self.master, text='get dangerous',command=self.obtener)
+        but.grid(row=0, column=3, padx=20, pady=30)
+
+        #return value.get()
+        #self.value = value
+        #self.value = value.get()
+        #value.get() = self.value
+        self.value = value
+   
     def obtener(self):
+    #def obtener(self):
+    	#print self.value
+    	value = self.value
     	print value.get()
-
-
-
-    def run(self):
-    	self.danger()
-    	self.box()
-    	self.obtener()
-
 
 root = tk.Tk()
 app = Application(root)
